cc33429d48d3cd378639aff3c7c39997de900e23
wu <1851848684@qq.com>
2022-12-10 09:30:30 +0800

    实现单页布局

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
 <template>
-  <router-view></router-view>
+  <SinglePageLayout/>
 </template>
+
+<script>
+import SinglePageLayout from './components/Layout/SinglePageLayout.vue';
+export default{
+  components:{
+    SinglePageLayout,
+  }
+}
+</script>
+
+<style>
+* {
+  padding:0;
+  margin:0;
+}
+html,body,#app{
+width: 100%;
+height: 100%;
+}
+</style>
---
+++ src/components/Layout/SinglePageLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+    <router-view></router-view>
+    </div>
+</template>
+
+<style>
+    .containe{
+        width:100%;
+        height:100%;
+    }
+</style>
