2e376e843430c8050d49dd365334cf89b8c50598
wu <1851848684@qq.com>
2023-01-03 23:47:36 +0800

    实现简历导出功能

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -2,6 +2,7 @@
   <div class="resume-container">
     <el-button v-if="editing" type="primary" @click="handleComplete">完成</el-button>
     <el-button v-else type="primary" @click="handleEdit">编辑</el-button>
+    <el-button @click="handleExportPdf">导出PDF</el-button>
     <h1>{{ info.name }}</h1>
     <ResumeList title="联系方式" :list="info.contact" :editing="editing"/>
     <ResumeList title="个人信息" :list="info.info" :editing="editing"/>
@@ -15,6 +16,8 @@
 <script>
 import ResumeList from './ResumeList.vue'
 import projectOrWorkExp from './projectOrWorkExp.vue'
+import html2canvas from 'html2canvas'
+import jspdf from 'jspdf'
 
 import defaultResumeInfo from './Resumeinfo'
 
@@ -43,9 +46,52 @@ export default {
     },
     handleComplete(){
       this.editing =false;
-
       localStorage.setItem('ResumeInfo' , JSON.stringify(this.info))
-    }
+    },
+    handleExportPdf(){
+    const pdf = new jspdf('', 'pt', 'a4')
+    const target = document.querySelector('.resume-container')
+
+    const clonedTarget = target.cloneNode(true);
+    clonedTarget.querySelectorAll('.el-button').forEach((button) => {
+      clonedTarget.removeChild(button)
+    })
+    document.body.appendChild(clonedTarget)
+    html2canvas(clonedTarget).then((canvas) => {
+
+      document.body.removeChild(clonedTarget)
+
+      const contentWidth = canvas.width
+      const contentHeight = canvas.height
+
+      //一页pdf显示html页面生成的canvas高度;
+      const pageHeight = (contentWidth / 595.28) * 841.89
+      //未生成pdf的html页面高度
+      let leftHeight = contentHeight
+      //页面偏移
+      let position = 0
+      //a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
+      const imgWidth = 595.28
+      const imgHeight = (592.28 / contentWidth) * contentHeight
+
+      const pageData = canvas.toDataURL('image/jpeg', 1.0)
+      if (leftHeight < pageHeight) {
+        pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight)
+        // 为下一条数据添加空白页
+      } else {
+        while (leftHeight > 0) {
+          pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
+          leftHeight -= pageHeight
+          position -= 841.89
+          //避免添加空白页
+          if (leftHeight > 0) {
+            pdf.addPage()
+          }
+        }
+      }
+      pdf.save('简历.pdf')
+    })
+    },
   }
 }
 </script>
